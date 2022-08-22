9d45f47afa94b366e597a19e6d842f14d62e08e0
Fedor Zhilkin <fzhilkin@mail.ru>
2022-08-22 22:21:01 +0300

    Change time

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,7 @@ def main():
             with st.spinner('Wait for it...'):
                 audio = AudioSegment.from_file(file_path)
                 duration = audio.duration_seconds
-                approximate_time = duration * 0.5
+                approximate_time = duration * 0.3
                 st.info(f"Approximate processing time: {str(datetime.timedelta(seconds=round(approximate_time)))}")
                 st.info('Transcribing...')
                 meeting_json = assembly_recognition.transcribe_meeting(
